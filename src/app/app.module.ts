ddf632ece193c066543fb7ac1789adf105c99e79
Gomupace <bblieck@hotmail.com>
2023-03-01 21:49:17 +0100

    added edit,delete and completed func

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    EditTodoDialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Todo } from '../shared/todo.model';
 import { TodoService } from '../shared/todo.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTodoDialogComponent } from '../edit-todo-dialog/edit-todo-dialog.component';
 
 @Component({
   selector: 'app-todos',
@@ -12,7 +14,7 @@ export class TodosComponent implements OnInit {
   todos!: Todo[];
   showValidationErrors!: boolean;
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.todos = this.todoService.getAllTodos();
@@ -25,5 +27,28 @@ export class TodosComponent implements OnInit {
     this.todoService.addTodo(new Todo(form.value.text));
     form.reset();
     return (this.showValidationErrors = false);
+  }
+
+  setCompleted(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  editTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '700px',
+      data: todo,
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.todoService.updateTodo(index, result);
+      }
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todoService.deleteTodo(index);
   }
 }
